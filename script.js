a3e770c6131294ac0bca982a306b24ef21f09814
suragsp <104720115+suragsp@users.noreply.github.com>
2023-08-29 09:32:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,15 @@ const wheel = document.getElementById('wheel');
 const numberDisplay = document.getElementById('number');
 const numberList = document.querySelectorAll('.number-list li');
 const spinButton = document.getElementById('spin-button');
+const chancesDisplay = document.getElementById('chances');
 
 let spinning = false;
+let chancesLeft = 5; // Set the initial number of chances
+
+chancesDisplay.textContent = `Chances left: ${chancesLeft}`;
 
 spinButton.addEventListener('click', () => {
-    if (spinning) return; // Prevent multiple spins
+    if (spinning || chancesLeft === 0) return; // Prevent spins when already spinning or no chances left
 
     const startAngle = 0;
     const endAngle = 360 * (numberList.length - 1);
@@ -15,6 +19,10 @@ spinButton.addEventListener('click', () => {
     const rotation = 1800 + randomAngle; // Rotate at least 5 full circles plus the random angle
 
     spinning = true;
+    chancesLeft--;
+
+    chancesDisplay.textContent = `Chances left: ${chancesLeft}`;
+
     wheel.style.transition = 'transform 5s ease-out';
     wheel.style.transform = `rotate(${rotation}deg)`;
 
@@ -25,7 +33,7 @@ spinButton.addEventListener('click', () => {
         const anglePerSegment = 360 / numberList.length;
         const targetSegmentIndex = Math.floor((randomAngle + anglePerSegment / 2) / anglePerSegment);
         const targetAngle = targetSegmentIndex * anglePerSegment;
-        
+
         // Rotate to the exact selected angle
         wheel.style.transition = 'transform 3s ease-in-out';
         wheel.style.transform = `rotate(${rotation + targetAngle}deg) scale(1.5)`;
