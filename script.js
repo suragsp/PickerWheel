6eb79789651cbf5be91fd285973bc06e530bd84b
suragsp <104720115+suragsp@users.noreply.github.com>
2023-08-29 09:27:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
 const wheel = document.getElementById('wheel');
 const numberDisplay = document.getElementById('number');
+const numberList = document.querySelectorAll('.number-list li');
 const spinButton = document.getElementById('spin-button');
 
+let spinning = false;
+
 spinButton.addEventListener('click', () => {
-    const startNumber = parseInt(prompt('Enter the start number:'));
-    const endNumber = parseInt(prompt('Enter the end number:'));
+    if (spinning) return; // Prevent multiple spins
 
-    if (isNaN(startNumber) || isNaN(endNumber)) {
-        alert('Please enter valid numbers.');
-        return;
-    }
+    const startAngle = 0;
+    const endAngle = 360 * (numberList.length - 1);
+    const randomAngle = Math.random() * (endAngle - startAngle) + startAngle;
 
-    const randomAngle = Math.random() * 360;
     const rotation = 1800 + randomAngle; // Rotate at least 5 full circles plus the random angle
 
-    wheel.style.transition = 'transform 3s ease-out';
+    spinning = true;
+    wheel.style.transition = 'transform 5s ease-out';
     wheel.style.transform = `rotate(${rotation}deg)`;
 
     setTimeout(() => {
-        const randomValue = Math.floor(Math.random() * (endNumber - startNumber + 1)) + startNumber;
-        numberDisplay.textContent = randomValue;
-        wheel.style.transition = 'none'; // Remove transition for zoom effect
-        wheel.style.transform = `rotate(${rotation}deg) scale(1.5)`;
-    }, 3000);
+        const selectedIndex = Math.floor(randomAngle / (360 / numberList.length));
+        const selectedNumber = numberList[selectedIndex].textContent;
+        numberDisplay.textContent = selectedNumber;
+        wheel.style.transition = 'transform 2s ease-out';
+        const correctedRotation = rotation + (360 / numberList.length / 2);
+        wheel.style.transform = `rotate(${correctedRotation}deg) scale(1.5)`;
+        setTimeout(() => {
+            wheel.style.transition = 'transform 1s ease-out';
+            wheel.style.transform = `rotate(${correctedRotation}deg) scale(1)`;
+            spinning = false;
+        }, 2000);
+    }, 5000);
 });
